7618240b63139b310f0e17ad07ae4987de684986
sreehari <sreehari229@gmail.com>
2022-09-11 20:23:08 +0530

    Signup Form

--- TaskMan/Doorway/urls.py
+++ TaskMan/Doorway/urls.py
@@ -1,6 +1,9 @@
+from unicodedata import name
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('create-acc/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
 ]
--- TaskMan/Doorway/views.py
+++ TaskMan/Doorway/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 def index(request):
     data = {
@@ -8,3 +9,15 @@ def index(request):
     }
     return render(request, 'Doorway/index_page.html', data)
 
+
+def signup(request):
+    data = {
+        'form' : UserCreationForm(),
+    }
+    return render(request, 'Doorway/signup.html', data)
+
+def login(request):
+    data = {
+        
+    }
+    return render(request, 'Doorway/login_page.html', data)
