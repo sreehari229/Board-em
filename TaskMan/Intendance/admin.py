33243975bee4681910d8c01418e3d14876f27412
sreehari <sreehari229@gmail.com>
2022-10-03 11:44:27 +0530

    I love you

--- TaskMan/Intendance/admin.py
+++ TaskMan/Intendance/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Profile
+from .models import *
 
 admin.site.register(Profile)
+admin.site.register(Project)
+admin.site.register(Project_Group)
+admin.site.register(Task)
+admin.site.register(Project_Invitation)
--- TaskMan/Intendance/models.py
+++ TaskMan/Intendance/models.py
@@ -1,8 +1,9 @@
 from email.policy import default
-import imp
+from enum import unique
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image as pilimg
+import uuid
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -17,7 +18,7 @@ class Profile(models.Model):
     account_modified_date = models.DateField(auto_now=True)
     
     def __str__(self):
-        return f'{self.user.username} Profile'
+        return f'{self.user.first_name} {self.user.last_name} - {self.user.username}'
     
     def save(self, *args, **kwargs):
         super(Profile, self).save()
@@ -26,3 +27,63 @@ class Profile(models.Model):
             output_size = (500,500)
             img.thumbnail(output_size)
             img.save(self.image.path)
+            
+
+
+class Project(models.Model):
+    project_id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    url = models.URLField(null=True, blank=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateField(auto_now_add=True)
+    start_date = models.DateField()
+    duration = models.IntegerField()
+    modified_date = models.DateField(auto_now=True)
+    
+    def __str__(self):
+        return f'{self.project_id} - {self.name}'
+    
+
+class Project_Group(models.Model):
+    project_id = models.OneToOneField(Project, on_delete=models.CASCADE)
+    group_members = models.ManyToManyField(User, blank=True)
+    created_date = models.DateField(auto_now_add=True)
+    modified_date = models.DateField(auto_now=True)
+    
+    def __str__(self):
+        return f'{self.project_id}'
+    
+
+class Task(models.Model):
+    status_choice = (
+        ('todo', 'ToDo'),
+        ('inprogress', 'In-Progress'),
+        ('testing', 'Testing'),
+        ('completed', 'Completed'),
+    )
+    task_id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)    
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    task_status = models.CharField(max_length=100, choices=status_choice, default='todo')
+    created_by = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=True, blank=True)
+    created_date = models.DateField(auto_now_add=True)
+    modified_date = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.title}'
+
+
+class Project_Invitation(models.Model):
+    invite_status = (
+        ('sent', 'Sent'),
+        ('accepted', 'Accepted'),
+        ('rejected', 'Rejected'),
+    )
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    sender = models.ForeignKey(User, null=False, related_name='Inviter', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(User, null=False, related_name='Invitee', on_delete=models.CASCADE)
+    status = models.CharField(max_length=100, choices=invite_status, default='sent')
+    created_date = models.DateField(auto_now_add=True)
+    modified_date = models.DateField(auto_now=True)
